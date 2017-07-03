b21e69aef36bd386b774def6bde94d45438311eb
Halvarsson Jonas <jonas.halvarsson@tieto.com>
2017-07-03 17:02:30 +0200

    webhook identifies intent

--- Webhook/index.js
+++ Webhook/index.js
@@ -1,7 +1,16 @@
-exports.helloHttp = function helloHttp (req, res) {
+//const ApiAiApp = require('actions-on-google').ApiAiApp;
 
-  //response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-  response = "The time is: " + new Date().toLocaleTimeString('sv-SE');
+exports.responseHandler = function responseHandler (req, res) {
+  //const application = new ApiAiApp({request: req, response: res});
+
+  //let intent = application.getIntent();
+  //let response = "You called intent: " + intent;
+
+  let intent = req.body.result.metadata.intentName;
+  let response = "You called intent: " + intent;
+
+  //response = "This is a sample response from your webhook!"; //Default response from the webhook to show it's working
+  //response = "The time is: " + new Date().toLocaleTimeString('sv-SE');
 
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
   res.send(JSON.stringify({ 
