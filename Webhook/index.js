12b4524fb3c4ea4f9c949f1396b281feded24e80
Halvarsson Jonas <jonas.halvarsson@tieto.com>
2017-07-05 16:24:25 +0200

    restructured webhook and started implementing illness

--- Webhook/index.js
+++ Webhook/index.js
@@ -1,39 +1,75 @@
 const ApiAiApp = require('actions-on-google').ApiAiApp;
+const children = ["Erik", "Jonas", "Alice"];
+const news = {};
 
-exports.responseHandler = function responseHandler (req, res) {
-  const application = new ApiAiApp({request: req, response: res});
+exports.schoolAgent = function schoolAgent (req, res) {
+  console.log('Headers: ' + JSON.stringify(req.headers));
+  console.log('Body: ' + JSON.stringify(req.body));
+
+  const app = new ApiAiApp({request: req, response: res});
 
   const TEST_INTENT = "hooktest.hello";
   const TIME_INTENT = "time.get";
-  const TEST_TEST = "test.test";
+  const LENGTH_INTENT = "length.get";
+  const ILLNESS_INTENT = "illness.register";
 
-  let intent = application.getIntent();
-  let response = "BIG ERROR";
+  const TEST_PROMPTS = ["This is a sample response from your webhook!", "You are now connected to the webhook!", "This is the webhook speaking :)"];
+  const ILLNESS_ASK_FOR_NAME_PROMPTS = ["Who is ill?", "Who is the poor sick bastard?"];
 
-  //let intent = req.body.result.metadata.intentName;
-  //let response = "You called intent: " + intent;
+  const NO_INPUT_PROMPTS = ["What was that?"];
+  const SICK_NO_INPUT_PROMPTS = ["Are you gonna call someone in sick or what??"];
 
-  switch (intent){
-    case TEST_INTENT:
-      response = "This is a sample response from your webhook!";
-      break;
-    case TIME_INTENT:
-      response = "The time is: " + new Date().toLocaleTimeString('sv-SE');
-      break;
-    case TEST_TEST:
-      response = application.getArgument("name") + " is " + Math.trunc(Math.random()*1000)/100 + "m long";
-      break;
-    default:
-      response = "DEFAULT CASE";
-      break;
+  let actionMap = new Map();
+  actionMap.set(TEST_INTENT, getSample);
+  actionMap.set(TIME_INTENT, getTime);
+  actionMap.set(LENGTH_INTENT, getLength);
+  actionMap.set(ILLNESS_INTENT, registerIllness);
+  app.handleRequest(actionMap);
+
+  function getSample(){
+    console.log('getSample');
+    let prompt = getRandomPrompt(app, TEST_PROMPTS);
+    tell(app, prompt, true);
   }
 
-  application.tell(response);
-  //application.ask(response);
-  
-  //res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  //res.send(JSON.stringify({ 
-  //    "speech": response, 
-  //    "displayText": response
-  //  }));
-};
+  function getTime(){
+    console.log('getTime');
+    let prompt = "The time is: " + new Date().toLocaleTimeString('sv-SE');
+    tell(app, prompt);    
+  }
+
+  function getLength(){
+    console.log('getLength');
+    let prompt = app.getArgument("name") + " is " + Math.trunc(Math.random()*1000)/100 + "m long";
+    tell(app, prompt);
+  }
+
+  function registerIllness(){
+    
+    tell(app, "You are now ill");
+  }
+
+  function getRandomPrompt(app, array, persist){
+    let lastPrompt = app.data.lastPrompt;
+    let prompt = "";
+    
+    do{
+      prompt = array[Math.floor(Math.random() * (array.length))];
+    } while(persist && lastPrompt === prompt && array.length > 1);
+
+    return prompt;
+  }
+
+  function ask(app, prompt, noInputs){
+    console.log('ask: ' + prompt);
+    app.data.lastPrompt = prompt;
+    app.ask(prompt, noInputs);
+  }
+
+  function tell(app, prompt){
+    console.log('tell: ' + prompt);
+    app.data.lastPrompt = prompt;
+    app.tell(prompt);
+  }
+
+}
